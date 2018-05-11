9074f4ae8eb2c692c55030564cc8c27cba054d4c
farimaf <farimaf@uci.edu>
2018-05-10 19:01:28 -0700

    sample-minor bug fix

--- src/Sample.java
+++ src/Sample.java
@@ -13,10 +13,11 @@ public class Sample {
     public static void main(String[] args) {
       Sample sampler = new Sample();
 //        sampler.sampleOneNode();
-        sampler.sampleOneNodeByCount(18195489,62751);//number of either clones or nonclones is given
+        sampler.sampleOneNodeByCount(18135188,62751);//number of either clones or nonclones is given
     }
 
-    private String pathInput = "/lv_scratch/scratch/mondego/local/farima/new_oreo/sampling_for_precision_related/input_clone_pairs_file/nonclones_type3_toolsunionfiltered.txt";
+    private String pathInput = "/lv_scratch/scratch/mondego/local/farima/new_oreo/sampling_for_precision_related/input_clone_pairs_file" +
+            "/nonclones_type3_toolsunionfiltered.txt";
     private PrintWriter printWriter;
 
     Sample() {
@@ -38,7 +39,7 @@ public class Sample {
                 randNumLine = random.nextInt(totalNumRows);
                 randomLineNums.add(randNumLine);
             }
-
+            System.out.println(randomLineNums.size());
             BufferedReader bufferedReader = new BufferedReader(new FileReader(pathInput));
             String line = "";
             int lineNum = 0;
@@ -48,6 +49,7 @@ public class Sample {
             }
             lineNum++;
             }
+            System.out.println(lineNum);
             System.out.println("Write Complete " );
             printWriter.close();
         } catch (Exception e) {
